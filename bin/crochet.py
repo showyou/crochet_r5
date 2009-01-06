ec58a8324584a92a3c4ffb51ffc038aceaef97a4
showyou <showyou41@gmail.com>
2009-01-06 21:55:29 +0900

    initial commit

---
+++ bin/crochet.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#! -*- coding:utf-8 -*-
+
+# crochetを起動するためのスクリプト
+class Crochet():
+	def __init__(self):
+		pass
+	def start(self):
+		pass
+
+if __name__ == "main":
+	c = Crochet()
+	c.start()
